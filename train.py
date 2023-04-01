cef48e8e8b53aa356023c2072d42d935b54da2a8
Rohit Yadav <edu.rohit.y@gmail.com>
2023-04-01 14:59:03 +0200

    Load dataset and train,test functions

--- train.py
+++ train.py
@@ -2,8 +2,17 @@
 
 import argparse
 
+from utils.preprocessing import ForestryDataset
+from utils.models import Models
+
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import segmentation_models_pytorch as smp
+
+
 parser = argparse.ArgumentParser(description="PyTorch BIP Segmentation")
-parser.add_argument("-data", "--path", metavar="DIR", 
+parser.add_argument("-data", "--path", metavar="DIR", default="../../datasets/treecover_segmentation_aerial_goettingen/",
                     help="Path to the dataset")
 parser.add_argument("-encoder", "--encoder", default="resnet50",
                     help="Encoder method to be used : ['resnet18', 'resnet34','resnet50', 'resnet101', 'resnet152','mobilenet_v2'] ")
@@ -22,7 +31,36 @@ parser.add_argument("-early_stop", action='store_true', default = False,
 
 def main():
     args = parser.parse_args()
-    
+    test_transformation = transforms.Compose([
+            transforms.ToTensor(),
+           # transforms.Resize(size=(512, 512)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225]),
+        ])
+    trainData = ForestryDataset(args.path, size = args.image_size, train = True, transform = None)
+    testData = ForestryDataset(args.path, size = args.image_size, train = False, transform = test_transformation)
+
+    # Train and test dataloaders
+    train_dataloader = DataLoader(trainData, batch_size=2, shuffle=True)
+    test_dataloader = DataLoader(testData, batch_size=1, shuffle=False)
+
+    # Load the model
+    model_obj = Models(args.method, args.encoder)
+    model = model_obj.getModel()
+
+    # Set device: `cuda` or `cpu`
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # define loss function
+    lossFunction = smp.utils.losses.DiceLoss()
+    # define metrics
+    metrics = [smp.utils.metrics.IoU(threshold=0.5)]
+    # define optimizer
+    optimizer = torch.optim.Adam([dict(params=model.parameters(), lr=args.lr,weight_decay=args.wd)])
+
+    # Get train and test functions 
+    train_epoch = model_obj.getTrainFunction(model, lossFunction, metrics, optimizer, device, True)
+    test_epoch = model_obj.getValidFunction(model, lossFunction, metrics, device, True)
+
 
 if __name__ == "__main__":
     main()
